cc495e3f8e058756ea241020f4a5bfeae67d8b6b
JoeyKraemer <joey.kramer@student.nhlstenden.com>
2022-10-06 13:47:42 +0200

    add pages

---
+++ app/public/menu.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en-US">
+   <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>OUI</title>
+      <link rel="stylesheet" href="style/styles.css" />
+   </head>
+   <body>
+      <header>
+         <nav></nav>
+      </header>
+      <main>
+
+      </main>
+      <footer>
+
+      </footer>
+   </body>
+</html> 
