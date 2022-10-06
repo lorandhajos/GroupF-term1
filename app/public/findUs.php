940e556ced1c20b7eb01109a0b2972545c598975
Lorand Hajos <lorand.hajos@student.nhlstenden.com>
2022-10-06 13:53:18 +0200

    CleanUp: fixed php files + camelCased image files

--- app/public/findUs.php
+++ app/public/findUs.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en-US">
    <head>
-      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>OUI</title>
-      <link rel="stylesheet" href="style/styles.css" />
+      <link rel="stylesheet" href="style/styles.css">
    </head>
    <body>
       <header>
@@ -17,4 +17,4 @@
 
       </footer>
    </body>
-</html> 
+</html>
