cea428550f6c0b7126182b4de13011371a4b9785
iamjames5 <lennox.james@student.nhlstenden.com>
2022-10-11 19:23:13 +0200

    Finished header

--- app/public/header.php
+++ app/public/header.php
@@ -1,12 +1,24 @@
+<!DOCTYPE html>
+<html>
 <link rel="stylesheet" href="style/styles.css">
-<header>
-   <img src="images/logo.svg" alt="logo" id="logo">
-   <nav>
-      <ul> 
-         <li>Menu</li>
-         <li>Find Us</li>
-         <li>About Us</li>
-         <li>Instruction</li>
-      </ul>
-   </nav>
-</header>
+	<body>
+		<head> 
+			<title> Oui Cafe</title>
+		</head>
+			 <header>
+				<div id="gridContainer">
+					<div class="header">
+						<img class="logo" src="images/logo.svg" alt="logo">
+							<div class="head">
+							  <ul> 
+								 <li>Menu</li>
+								 <li>Find Us</li>
+								 <li>About Us</li>
+								 <li>Instruction</li>
+							  </ul>
+							</div>
+					</div>
+				</div>
+		  </header>
+	</body>
+</html>
