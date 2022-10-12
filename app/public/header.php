0b9dd7be7eb9ad7172ee2e9f3ca03468337a09e5
JoeyKraemer <joey.kramer@student.nhlstenden.com>
2022-10-12 12:58:53 +0200

    Fixed error in Code

--- app/public/header.php
+++ app/public/header.php
@@ -1,24 +1,17 @@
 <!DOCTYPE html>
 <html>
 <link rel="stylesheet" href="style/styles.css">
-	<body>
-		<head> 
-			<title> Oui Cafe</title>
-		</head>
-			 <header>
-				<div id="gridContainer">
-					<div class="header">
-						<img class="logo" src="images/logo.svg" alt="logo">
-							<div class="head">
-							  <ul> 
-								 <li>Menu</li>
-								 <li>Find Us</li>
-								 <li>About Us</li>
-								 <li>Instruction</li>
-							  </ul>
-							</div>
-					</div>
-				</div>
-		  </header>
-	</body>
-</html>
+
+<div id="gridContainer">
+	<header>
+		<img class="logo" src="images/logo.svg" alt="logo">
+		<nav>
+			<ul> 
+				<li>Menu</li>
+				<li>Find Us</li>
+				<li>About Us</li>
+				<li>Instruction</li>
+			</ul>
+		</nav	>
+	</header>
+</div>
