2d42c6a8da6bb23a5eafb838c2596322092fd0e7
iamjames5 <lennox.james@student.nhlstenden.com>
2022-10-13 15:45:40 +0200

    Errors in comments fixed

--- app/public/header.php
+++ app/public/header.php
@@ -1,18 +1,19 @@
 <!DOCTYPE html>
 <html>
-<link rel="stylesheet" href="style/styles.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Caveat">
+   <link rel="stylesheet" href="style/styles.css">
+   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Caveat">
 
-<div class="gridContainer">
-	<header>
-		<img class="logo" src="images/logo.svg" alt="logo">
-		<nav>
-			<ul> 
-				<li><a href="menu.php">Menu</a></li>
-				<li><a href="findUs.php">Find Us</a></li>
-				<li><a href="aboutUs.php">About Us</a></li>
-				<li><a href="instruction.php">Instruction</a></li>
-			</ul>
-		</nav>
-	</header>
-</div>
+   <div class="gridContainer">
+      <header>
+         <img class="logo" src="images/logo.svg" alt="logo">
+         <nav>
+            <ul> 
+               <li><a href="menu.php">Menu</a></li>
+               <li><a href="findUs.php">Find Us</a></li>
+               <li><a href="aboutUs.php">About Us</a></li>
+               <li><a href="instruction.php">Instruction</a></li>
+            </ul>
+         </nav>
+      </header>
+   </div>
+</html>
