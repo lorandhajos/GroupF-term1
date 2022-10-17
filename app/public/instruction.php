384d816974c15358794bce6c08e1e22f448d7f39
iamjames5 <lennox.james@student.nhlstenden.com>
2022-10-17 10:53:48 +0200

    instruction page completed

--- app/public/instruction.php
+++ app/public/instruction.php
@@ -35,6 +35,18 @@
                      <a href="#" class="button">Learn More</a>
                   </div>
                </div>
+                  <h2>Lessons</h2>
+               <div class="content">
+                  <div>
+                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nisi augue, euismod id purus sed, dignissim dignissim arcu. Nunc facilisis purus a dignissim faucibus. 
+                        Pellentesque nec semper sapien. Quisque elementum quam metus, sit amet placerat nulla vulputate eu. Sed nec volutpat elit, sed mattis nunc. 
+                        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inc
+                     </p>
+                     <div class="signUp">
+                        <a href="#" class="signupButton">Sign Up</a>
+                     </div>
+                  </div>
+               </div>
             </section>
          </main>
          <footer>
