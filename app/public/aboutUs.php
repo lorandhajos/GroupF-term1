d3a91ef859fec858094af8f33fb3eb1241464ff2
Grzegorz Tomkiel <grzegorz.tomkiel@student.nhlstenden.com>
2022-10-19 02:30:40 +0200

    About Us page content + CSS styling

--- app/public/aboutUs.php
+++ app/public/aboutUs.php
@@ -7,14 +7,56 @@
       <link rel="stylesheet" href="style/styles.css">
    </head>
    <body>
-      <header>
-         <nav></nav>
-      </header>
-      <main>
+      <div class="gridContainer">
+         <header>
+            <img src="images/logo.svg" alt="logo" id="logo">
+            <nav></nav>
+         </header>
+         <main>
+            <section id="aboutUs">
+               <h1>About Us</h1>
+               <div id="line1">
+                  <p>Here at café OUI we pride
+                     ourselves on our homemade
+                     products and authentic French
+                     café experience.
 
-      </main>
-      <footer>
+                     Café OUI provides a calm and 
+                     relaxing environment for all
+                     visitors where they can enjoy 
+                     their coffee and sweets of
+                     choice. </p>
+                  <img id="pic1" src="images/coffeeCroissant.jpg" alt="line1 pic1">
+                  <img id="pic2" src="images/pastries.jpg" alt="line1 pic2">
+               </div>
+               <div id="line2">
+                  <p>Our goal for our customers is to provide the experience of walking into an authentic French café on the countryside.</p> 
+               </div>
 
-      </footer>
+               <div id="line3">
+                  <img src="images/macaron1.jpg">
+                  <p>Using our specific choices
+                     regarding decorations, furniture 
+                     and line of products, our 
+                     customers are provided with an 
+                     authentic feel of serenity and 
+                     French hospitality.
+
+                     Please try our macarons while 
+                     enjoying a fresh cup of French 
+                     Roast coffee, all homemade in 
+                     our very own café.</p>
+               </div>
+
+               <div id="line4">
+                  <p>When you leave café OUI, we are sure that you will say oui oui, tout suite when we say au revoir.</p>
+               </div>
+            </section>
+         </main>
+         <footer>
+            <p>The bottom of the page</p>
+         </footer>
+      </div>
    </body>
 </html>
+
