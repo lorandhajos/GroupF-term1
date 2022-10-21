f4f43c06ab145eb0b9acf5e78e43f256b39754c5
lorandhajos <lorand.hajos@student.nhlstenden.com>
2022-10-21 13:41:27 +0200

    Updated lesson structure

--- app/public/instruction.php
+++ app/public/instruction.php
@@ -36,14 +36,13 @@
                   </div>
                </div>
             </section>
-			<section class="lessonContent">
+            <section class="lessonContent">
                <h2>Lessons</h2>
                <div>
                   <div>
                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nisi augue, euismod id purus sed, dignissim dignissim arcu. Nunc facilisis purus a dignissim faucibus. 
                         Pellentesque nec semper sapien. Quisque elementum quam metus, sit amet placerat nulla vulputate eu. Sed nec volutpat elit, sed mattis nunc. 
-                        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inc
-                     </p>
+                        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inc</p>
                      <div class="signUp">
                         <a href="#" class="signupButton">Sign Up</a>
                      </div>
