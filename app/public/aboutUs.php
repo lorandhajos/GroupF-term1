4f2d9c32b3d242d14e77dde78df8e408db208bf1
Grzegorz Tomkiel <grzegorz.tomkiel@student.nhlstenden.com>
2022-10-24 15:07:22 +0200

    Minor code refactor

--- app/public/aboutUs.php
+++ app/public/aboutUs.php
@@ -16,40 +16,40 @@
             <section id="aboutUs">
                <h1>About Us</h1>
                <div id="line1">
-                  <p>Here at café OUI we pride
-                     ourselves on our homemade
-                     products and authentic French
-                     café experience.
-
-                     Café OUI provides a calm and 
-                     relaxing environment for all
-                     visitors where they can enjoy 
-                     their coffee and sweets of
-                     choice. </p>
+                  <div id="line1text">
+                     <p>Here at café OUI we pride
+                        ourselves on our homemade
+                        products and authentic French
+                        café experience.</p>
+                     <p>Café OUI provides a calm and 
+                        relaxing environment for all
+                        visitors where they can enjoy 
+                        their coffee and sweets of
+                        choice.</p>
+                  </div>
                   <img id="pic1" src="images/coffeeCroissant.jpg" alt="line1 pic1">
                   <img id="pic2" src="images/pastries.jpg" alt="line1 pic2">
                </div>
                <div id="line2">
-                  <p>Our goal for our customers is to provide the experience of walking into an authentic French café on the countryside.</p> 
+                  <h3>Our goal for our customers is to provide the experience of walking into an authentic French café on the countryside.</h3>
                </div>
-
                <div id="line3">
                   <img src="images/macaron1.jpg" alt="line3 pic">
-                  <p>Using our specific choices
-                     regarding decorations, furniture 
-                     and line of products, our 
-                     customers are provided with an 
-                     authentic feel of serenity and 
-                     French hospitality.
-
-                     Please try our macarons while 
-                     enjoying a fresh cup of French 
-                     Roast coffee, all homemade in 
-                     our very own café.</p>
+                  <div id="line3text">
+                     <p>Using our specific choices
+                        regarding decorations, furniture 
+                        and line of products, our 
+                        customers are provided with an 
+                        authentic feel of serenity and 
+                        French hospitality.</p>
+                     <p>Please try our macarons while 
+                        enjoying a fresh cup of French 
+                        Roast coffee, all homemade in 
+                        our very own café.</p>
+                  </div>
                </div>
-
                <div id="line4">
-                  <p>When you leave café OUI, we are sure that you will say oui oui, tout suite when we say au revoir.</p>
+                  <h3>When you leave café OUI, we are sure that you will say oui oui, tout suite when we say au revoir.</h3>
                </div>
             </section>
          </main>
