78a17db586f4b0549064f05d46678051fa8269b9
iarina dicu <iarina.dicu@student.nhlstenden.com>
2022-10-24 15:03:21 +0200

    menu

--- app/public/menu.php
+++ app/public/menu.php
@@ -11,7 +11,37 @@
          <nav></nav>
       </header>
       <main>
-
+         <div class = "box">
+            <div class = "drinks">
+                <p class = "drinkText">Drinks</p>
+                <div class = "drinkContext">
+                    <p class ="drinkText2"> Product 1 ...................... 2.50 €
+                        Product 2 ..................... 3.00 €
+                        Product 3 ..................... 1.50 €
+                        Product 4 ..................... 2.00 €
+                        Product 5 ..................... 2.00 €
+                        Product 6 ..................... 2.00 €
+                        Product 7 ..................... 2.00 €
+                        Product 8 ..................... 2.00 €
+                    </p>
+                </div>
+            </div>
+            <div class = "bakedGods">
+                <p class="bakedGodsText"> Baked Gods</p>
+                <div class="bakedGodsContext">
+                    <p class="bakedGodsText2">
+                        Product 1 ...................... 2.50 €
+                        Product 2 ..................... 3.00 €
+                        Product 3 ..................... 1.50 €
+                        Product 4 ..................... 2.00 €
+                        Product 5 ..................... 2.00 €
+                        Product 6 ..................... 2.00 €
+                        Product 7 ..................... 2.00 €
+                        Product 8 ..................... 2.00 €
+                    </p>
+                </div>
+            </div>
+        </div>
       </main>
       <footer>
 
