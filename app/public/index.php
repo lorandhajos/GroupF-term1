7d6dea785b9ea0db0cc2d0a7534c496f5485c7c1
Lorand Hajos <lorand.hajos@student.nhlstenden.com>
2022-10-24 16:19:04 +0200

    added include header.php

--- app/public/index.php
+++ app/public/index.php
@@ -15,15 +15,7 @@
    <body>
       <div class="gridContainer">
          <header>
-            <img class="logo" src="images/logo.svg" alt="logo">
-            <nav>
-               <ul> 
-                  <li><a href="index.php?page=menu">Menu</a></li>
-                  <li><a href="index.php?page=findUs">Find Us</a></li>
-                  <li><a href="index.php?page=aboutUs">About Us</a></li>
-                  <li><a href="index.php?page=instruction">Instruction</a></li>
-               </ul>
-            </nav>
+            <?php include 'header.php'; ?>
          </header>
          <main>
             <?php
