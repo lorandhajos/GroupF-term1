bd69598a9f880a592ac3e839b3f4d222febcb407
Hakan Ege Ozerol <hakan.ege.ozerol@student.nhlstenden.com>
2022-10-24 21:00:38 +0200

    fixed the hero banner

--- app/public/index.php
+++ app/public/index.php
@@ -1,58 +1,56 @@
 <!DOCTYPE html>
 <html lang="en-US">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>OUI</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-<div class="gridContainer">
-    <header>
-        <img src="images/logo.svg" alt="logo" id="logo">
-        <nav>
-            <ul>
-                <li>Menu</li>
-                <li>Find Us</li>
-                <li>About Us</li>
-                <li>Instruction</li>
-            </ul>
-        </nav>
-    </header>
-    <main>
-        <!-- heroBanner -->
-        <section class="heroBanner">
-			<h1>café avec une touche française</h1>
-        </section>
-		<section class="products">
-			<h2>Our french products</h2>
-			<div class="product">
-				<img src="images/croissant1.jpg" alt="croissant francais">
-				<h3>Croissant</h3>
-			</div>
-			<div class="product">
-				<img src="images/coffeeBeans1.jpg" alt="coffee francais">
-				<h3>Coffee</h3>
-			</div>
-			<div class="product">
-				<img src="images/baguette.jpeg" alt="baguette francais">
-				<h3>Baguette</h3>
-			</div>
-			<div class="product">
-				<img src="images/macaron1.jpg" alt="macaron francais">
-				<h3>Macaron</h3>
-			</div>
-		</section>
-        <!-- findUs -->
-        <section class="findUS">
-			<p>TESTI TEST</p>
-        </section>
-		
-        
-    </main>
-    <footer>
-        <p>The bottom of the page</p>
-    </footer>
-</div>
-</body>
+   <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>OUI</title>
+      <link rel="stylesheet" href="style/styles.css">
+   </head>
+   <body>
+      <div class="gridContainer">
+         <header>
+            <img src="images/logo.svg" alt="logo" id="logo">
+            <nav>
+               <ul>
+                  <li>Menu</li>
+                  <li>Find Us</li>
+                  <li>About Us</li>
+                  <li>Instruction</li>
+               </ul>
+            </nav>
+         </header>
+         <main>
+            <!-- heroBanner -->
+            <section class="heroBanner">
+               <h1>café avec une touche française</h1>
+            </section>
+            <section class="products">
+               <h2>Our french products</h2>
+               <div class="product">
+                  <img src="images/croissant1.jpg" alt="croissant francais">
+                  <h3>Croissant</h3>
+               </div>
+               <div class="product">
+                  <img src="images/coffeeBeans1.jpg" alt="coffee francais">
+                  <h3>Coffee</h3>
+               </div>
+               <div class="product">
+                  <img src="images/baguette.jpeg" alt="baguette francais">
+                  <h3>Baguette</h3>
+               </div>
+               <div class="product">
+                  <img src="images/macaron1.jpg" alt="macaron francais">
+                  <h3>Macaron</h3>
+               </div>
+            </section>
+            <!-- findUs -->
+            <section class="findUS">
+               <p>TESTI TEST</p>
+            </section>
+         </main>
+         <footer>
+            <p>The bottom of the page</p>
+         </footer>
+      </div>
+   </body>
 </html>
