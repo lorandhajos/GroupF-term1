6835e79f8c596de9d257e5f06f44a32f5a95eeff
iarina dicu <iarina.dicu@student.nhlstenden.com>
2022-10-25 11:51:02 +0200

    menu chnage

--- app/public/menu.php
+++ app/public/menu.php
@@ -11,34 +11,35 @@
          <nav></nav>
       </header>
       <main>
-         <div class = "box">
-            <div class = "drinks">
-                <p class = "drinkText">Drinks</p>
-                <div class = "drinkContext">
-                    <p class ="drinkText2"> Product 1 ...................... 2.50 €
-                        Product 2 ..................... 3.00 €
-                        Product 3 ..................... 1.50 €
-                        Product 4 ..................... 2.00 €
-                        Product 5 ..................... 2.00 €
-                        Product 6 ..................... 2.00 €
-                        Product 7 ..................... 2.00 €
-                        Product 8 ..................... 2.00 €
-                    </p>
+         <div class="box">
+            <div class="drinks">
+                <p class="drinkText">Drinks</p>
+                <div class="drinkContext">
+				   <div class="menuGrid">
+						<p>Cappuccino</p><p class="lastMenu">2.50 €</p>
+                        <p>Expresso Double</p><p class="lastMenu">3.00 €</p>
+                        <p>Latte</p><p class="lastMenu">1.50 €</p>
+                        <p>The Chaud</p><p class="lastMenu">2.00 €</p>
+                        <p>The Glace</p><p class="lastMenu">2.00 €</p>
+                        <p>Un Soda</p><p class="lastMenu">2.00 €</p>
+                        <p>Le Lait</p><p class="lastMenu">2.00 €</p>
+                        <p>Lait au Chocolat</p><p class="lastMenu">2.00 €</p>
+					</div>
                 </div>
             </div>
-            <div class = "bakedGods">
-                <p class="bakedGodsText"> Baked Gods</p>
-                <div class="bakedGodsContext">
-                    <p class="bakedGodsText2">
-                        Product 1 ...................... 2.50 €
-                        Product 2 ..................... 3.00 €
-                        Product 3 ..................... 1.50 €
-                        Product 4 ..................... 2.00 €
-                        Product 5 ..................... 2.00 €
-                        Product 6 ..................... 2.00 €
-                        Product 7 ..................... 2.00 €
-                        Product 8 ..................... 2.00 €
-                    </p>
+            <div class="bakedGoods">
+                <p class="bakedGoodsText">Baked Goods</p>
+                <div class="bakedGoodsContext">
+                    <div class="menuGrid">
+                        <p>Le Petit Dejeuner</p><p class="lastMenu">2.50 €</p>
+						<p>Croissant au Jambon</p><p class="lastMenu">3.00 €</p>
+						<p>Croissant</p><p class="lastMenu">1.50 €</p>
+						<p>Pain au Chocolat</p><p class="lastMenu">2.00 €</p>
+						<p>Croissant aux Pommes</p><p class="lastMenu">2.00 €</p>
+						<p>Tartare de Fruits</p><p class="lastMenu">2.00 €</p>
+						<p>Macarons</p><p class="lastMenu">2.00 €</p>
+						<p>Baguette</p><p class="lastMenu">2.00 €</p>
+                    </div>
                 </div>
             </div>
         </div>
