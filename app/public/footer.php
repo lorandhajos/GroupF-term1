4eeb2596a373a2a34db5237f13db09844f4661b0
Andrii Chumakov <andrii.chumakov@student.nhlstenden.com>
2022-10-25 10:31:05 +0200

    Experimental wide screen version

--- app/public/footer.php
+++ app/public/footer.php
@@ -6,42 +6,48 @@
    </head>
    <body class="gridContainer">
       <footer>
-         <div>
-            <a href="https://www.facebook.com" class="footerLink">
-               <img src="images/facebook.svg" alt="facebook.com">
-            </a>
-            <a href="https://www.twitter.com" class="footerLink">
-               <img src="images/twitter.svg" alt="twitter.com">
-            </a>
-            <a href="https://www.instagram.com" class="footerLink">
-               <img src="images/instagram.svg" alt="instagram.com">
-            </a>
-         </div>
-         <div>
-            <h4><b>Working Hours:</b></h4>
-            <p>8:00-16:00 Mon-Fri</p>
-            <p>9:00-13:00 Sat-Sun</p>
-         </div>
-         <div class="verticalLine"> 
-            <h4><b>Menu</b></h4>
-         </div>
-         <div>
-            <p><a href="menu.php" class="footerLink">Menu</a></p>
-            <p><a href="findUs.php" class="footerLink">Find Us</a></p>
-            <p><a href="aboutUs.php" class="footerLink">About Us</a></p>
-            <p><a href="instructions.php" class="footerLink">Instructions</a></p>
-            <p></p>
-         </div>
-         <div class="verticalLine"> 
-            <h4><b>Contact</b></h4>
-         </div>
-         <div>
-            <p>OUI cafe</p>
-            <p>1 Main Rd.</p>
-            <p></p>
-            <p>Phone: +1111111111</p>
-            <p>Email: contact@oui.com</p>
-         </div>
+         <div id="footerFlex">
+            <div>
+               <a href="https://www.facebook.com" class="footerLink">
+                  <img src="images/facebook.svg" alt="facebook.com">
+               </a>
+               <a href="https://www.twitter.com" class="footerLink">
+                  <img src="images/twitter.svg" alt="twitter.com">
+               </a>
+               <a href="https://www.instagram.com" class="footerLink">
+                  <img src="images/instagram.svg" alt="instagram.com">
+               </a>
+            </div>
+            <div>
+               <h4><b>Working Hours:</b></h4>
+               <p>8:00-16:00 Mon-Fri</p>
+               <p>9:00-13:00 Sat-Sun</p>
+            </div>
+            <div class="textContainer">
+               <div class="verticalLine"> 
+                  <h4><b>Menu</b></h4>
+               </div>
+               <div>
+                  <p><a href="menu.php" class="footerLink">Menu</a></p>
+                  <p><a href="findUs.php" class="footerLink">Find Us</a></p>
+                  <p><a href="aboutUs.php" class="footerLink">About Us</a></p>
+                  <p><a href="instructions.php" class="footerLink">Instructions</a></p>
+                  <p></p>
+               </div>
+            </div>
+            <div class="textContainer">
+               <div class="verticalLine"> 
+                  <h4><b>Contact</b></h4>
+               </div>
+               <div>
+                  <p>OUI cafe</p>
+                  <p>1 Main Rd.</p>
+                  <p></p>
+                  <p>Phone: +1111111111</p>
+                  <p>Email: contact@oui.com</p>
+               </div>
+            </div>
+            </div>
          <div>
             <h5>© 2022 OUI. All rights reserved.</h5>
          </div>
