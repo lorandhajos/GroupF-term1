76a48f200466cf4f01ea11b85552a70444b89880
Lorand Hajos <lorand.hajos@student.nhlstenden.com>
2022-10-26 13:18:15 +0200

    fixed menu.php to work with the page switcher

--- app/public/menu.php
+++ app/public/menu.php
@@ -1,50 +1,32 @@
-<!DOCTYPE html>
-<html lang="en-US">
-<head>
-   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>OUI</title>
-   <link rel="stylesheet" href="style/styles.css">
-</head>
-<header>
-   <nav></nav>
-</header>
-<main>
-   <div class="box">
-      <div class="drinks">
-         <p class="drinkText">Drinks</p>
-         <div class="drinkContext">
-            <div class="menuGrid">
-               <p>Cappuccino</p><p class="lastMenu">2.50 €</p>
-               <p>Expresso Double</p><p class="lastMenu">3.00 €</p>
-               <p>Latte</p><p class="lastMenu">1.50 €</p>
-               <p>The Chaud</p><p class="lastMenu">2.00 €</p>
-               <p>The Glace</p><p class="lastMenu">2.00 €</p>
-               <p>Un Soda</p><p class="lastMenu">2.00 €</p>
-               <p>Le Lait</p><p class="lastMenu">2.00 €</p>
-               <p>Lait au Chocolat</p><p class="lastMenu">2.00 €</p>
-            </div>
-         </div>
-      </div>
-      <div class="bakedGoods">
-         <p class="bakedGoodsText">Baked Goods</p>
-         <div class="bakedGoodsContext">
-            <div class="menuGrid">
-               <p>Le Petit Dejeuner</p><p class="lastMenu">2.50 €</p>
-               <p>Croissant au Jambon</p><p class="lastMenu">3.00 €</p>
-               <p>Croissant</p><p class="lastMenu">1.50 €</p>
-               <p>Pain au Chocolat</p><p class="lastMenu">2.00 €</p>
-               <p>Croissant aux Pommes</p><p class="lastMenu">2.00 €</p>
-               <p>Tartare de Fruits</p><p class="lastMenu">2.00 €</p>
-               <p>Macarons</p><p class="lastMenu">2.00 €</p>
-               <p>Baguette</p><p class="lastMenu">2.00 €</p>
-            </div>
+<div class="box">
+   <div class="drinks">
+      <p class="drinkText">Drinks</p>
+      <div class="drinkContext">
+         <div class="menuGrid">
+            <p>Cappuccino</p><p class="lastMenu">2.50 €</p>
+            <p>Expresso Double</p><p class="lastMenu">3.00 €</p>
+            <p>Latte</p><p class="lastMenu">1.50 €</p>
+            <p>The Chaud</p><p class="lastMenu">2.00 €</p>
+            <p>The Glace</p><p class="lastMenu">2.00 €</p>
+            <p>Un Soda</p><p class="lastMenu">2.00 €</p>
+            <p>Le Lait</p><p class="lastMenu">2.00 €</p>
+            <p>Lait au Chocolat</p><p class="lastMenu">2.00 €</p>
          </div>
       </div>
    </div>
-</main>
-<footer>
-
-</footer>
-
-</html>
+   <div class="bakedGoods">
+      <p class="bakedGoodsText">Baked Goods</p>
+      <div class="bakedGoodsContext">
+         <div class="menuGrid">
+            <p>Le Petit Dejeuner</p><p class="lastMenu">2.50 €</p>
+            <p>Croissant au Jambon</p><p class="lastMenu">3.00 €</p>
+            <p>Croissant</p><p class="lastMenu">1.50 €</p>
+            <p>Pain au Chocolat</p><p class="lastMenu">2.00 €</p>
+            <p>Croissant aux Pommes</p><p class="lastMenu">2.00 €</p>
+            <p>Tartare de Fruits</p><p class="lastMenu">2.00 €</p>
+            <p>Macarons</p><p class="lastMenu">2.00 €</p>
+            <p>Baguette</p><p class="lastMenu">2.00 €</p>
+         </div>
+      </div>
+   </div>
+</div>
