e97463b5ee68e28613575a0bae73101d35368592
Andrii Chumakov <andrii.chumakov@student.nhlstenden.com>
2022-10-26 21:38:59 +0200

    added instruction banner to the home page

---
+++ app/public/home.php
@@ -0,0 +1,14 @@
+<section class="homeInstructionsBox">
+	<h2 class="instructionsTitle">Instructions</h2>
+	<div class="instructionsFlex">
+		<img src="images/bakeCroissant.jpg" class="bakeCroissantsImg">
+		<div class="textImgFlex">
+			<div>
+				<p>Want to bake croissants like we do?</p>
+				<p>Then sign up for our lessons.</p>
+			<!--insert the lessons page into location.href = '' -->
+			</div>
+			<button type="button" class="toLessonsBtn" onclick="location.href='index.php?page=home'">Go to lessons</button>
+		</div>
+	</div>
+</section>
