bd764d4988cf979e5c045793138d90ec2f7882de
Iarina <112045565+iarinamd@users.noreply.github.com>
2022-10-26 13:52:49 +0200

    menu page en

---
+++ app/public/menu_en.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>OUI</title>
+   <link rel="stylesheet" href="style/styles.css">
+</head>
+<header>
+   <nav></nav>
+</header>
+<main>
+   <div class="box">
+      <div class="drinks">
+         <p class="drinkText">Drinks</p>
+         <div class="drinkContext">
+            <div class="menuGrid">
+               <p>Cappuccino</p><p class="lastMenu">2.50 €</p>
+               <p>Double Espresso</p><p class="lastMenu">3.00 €</p>
+               <p>Latte</p><p class="lastMenu">1.50 €</p>
+               <p>Hot Tea</p><p class="lastMenu">2.00 €</p>
+               <p>Iced Tea</p><p class="lastMenu">2.00 €</p>
+               <p>Soda</p><p class="lastMenu">2.00 €</p>
+               <p>Milk</p><p class="lastMenu">2.00 €</p>
+               <p>Chocolate Milk</p><p class="lastMenu">2.00 €</p>
+            </div>
+         </div>
+      </div>
+      <div class="bakedGoods">
+         <p class="bakedGoodsText">Baked Goods</p>
+         <div class="bakedGoodsContext">
+            <div class="menuGrid">
+               <p>Breakfast</p><p class="lastMenu">2.50 €</p>
+               <p>Ham Croissant</p><p class="lastMenu">3.00 €</p>
+               <p>Croissant</p><p class="lastMenu">1.50 €</p>
+               <p>Chocolate Croissant</p><p class="lastMenu">2.00 €</p>
+               <p>Apple Croissant</p><p class="lastMenu">2.00 €</p>
+               <p>Fruits Tart</p><p class="lastMenu">2.00 €</p>
+               <p>Macarons</p><p class="lastMenu">2.00 €</p>
+               <p>Baguette</p><p class="lastMenu">2.00 €</p>
+            </div>
+         </div>
+      </div>
+   </div>
+</main>
+<footer>
+
+</footer>
+
+</html>
