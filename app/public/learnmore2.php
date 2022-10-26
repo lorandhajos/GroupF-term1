df0026cfe2873fa5785c1e0669295e7852fc5327
Iarina <112045565+iarinamd@users.noreply.github.com>
2022-10-26 23:16:18 +0200

    first page of learn more 1

---
+++ app/public/learnmore2.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>OUI</title>
+   <link rel="stylesheet" href="style/styles2.css">
+</head>
+<header>
+   <nav></nav>
+</header>
+
+   <div class="containerLearnMore2">
+   <div class = "box2">
+      <div class="ingredients2">
+         <h1 class="recipieText12">Ingredients</h1>
+            <div class="recipieIngredients2">
+               <p><b>1-1/2 cups</b> warm milk</p>
+               <p><b>1/4 cup</b> light brown sugar</p>
+               <p><b>1+1/4 teaspoon</b> dry yeast</p>
+               <p><b>3-4 cups</b> flour</p>
+               <p><b>1 teaspoon</b> salt</p>
+               <p><b>1-1/2 cups</b> cold water</p>
+            </div>
+      </div>
+      <div class="preparation2">
+         <h2 class="recipieText22">How to make</h2>
+            <div class="recipiePreparation2">
+               <p style="font-size:27px"><b><i>Step One</i></b></p>
+               <p>Stir together warm milk, brown sugar, and yeast in bowl of standing mixer and let stand until foamy, about 5 minutes.</p>
+               <p>Add 3 3/4 cups flour and salt and mix with dough hook at low speed until dough is smooth and very soft, about 7 minutes.</p>
+               <p style="font-size:27px"><b><i>Step Two</i></b></p>
+               <p>Transfer dough to a work surface and knead by hand 2 minutes, adding more flour as necessary, a little at a time, to make a soft, slightly sticky dough.</p>
+               <p>Form dough into a roughly 1-1/2-inch-thick rectangle and chill, wrapped in plastic wrap, until cold, about 1 hour.</p>
+               <p style="font-size:27px"><b><i>Step Three</i></b></p>
+               <p>After dough has chilled, arrange sticks of butter horizontally, their sides touching, on a work surface. Pound butter with a rolling pin to soften slightly</p>
+               <p>Scrape butter into a block and put on a kitchen towel, then cover with other towel. Pound and roll out on both sides until butter forms a uniform 8 by 5 inch rectangle.</p>
+               <p>Chill, wrapped in towels, while rolling out dough.</p>
+               <p style="font-size:27px"><b><i>Step Four</i></b></p>
+               <p>Unwrap dough and roll out on a lightly floured surface, dusting with flour as necessary and lifting and stretching dough into a 16 by 10 inch rectangle.</p>
+               <p> Arrange dough with a short side nearest you. Put butter in center of dough so that long sides of butter are parallel to short sides of dough. </p>
+               <p> Fold as you would a letter: bottom third of dough over butter, then top third down over dough. Brush off excess flour with pastry brush.</p>
+               <p style="font-size:27px"><b><i>Step Five</i></b></p>
+               <p>Turn dough so a short side is nearest you, then flatten dough slightly by pressing down horizontally with rolling pin across dough at regular intervals, making uniform impressions.</p>
+               <p>Roll out dough into a 15 by 10 inch rectangle, rolling just to but not over ends.</p>
+               <p style="font-size:27px"><b><i>Step Six</i></b></p>
+               <p>Brush off any excess flour. Fold in thirds like a letter, as above, stretching corners to square off dough, forming a 10 by 5 inch rectangle.</p>
+               <p>Chill, wrapped in plastic wrap, 1 hour.</p>
+               <p style="font-size:27px"><b><i>Step Seven</i></b></p>
+               <p>Make 3 more folds in same manner, chilling dough 1 hour after each fold, for a total of 4 folds.</p>
+               <p>Wrap dough tightly in plastic wrap and chill at least 8 hours but no more than 18</p>
+            </div>
+      </div>
+      </div>
+      <div class="video2">
+         <iframe width="560" height="315" src="https://www.youtube.com/embed/hJxaVD6eAtc" title="YouTube video player" frameborder="0" 
+         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+         </iframe>
+      </div>
+   </div>
+<footer>
+
+</footer>
+
+</html>
