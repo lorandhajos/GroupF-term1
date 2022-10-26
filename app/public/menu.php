c369aca572978075b91fce75fd6e3c334c6263c9
Lorand Hajos <lorand.hajos@student.nhlstenden.com>
2022-10-26 14:42:16 +0200

    fixed menu page pt.1

--- app/public/menu.php
+++ app/public/menu.php
@@ -1,34 +1,58 @@
 <?php if(!defined("OUI")) exit; ?>
 
-<div class="box">
-   <div class="drinks">
-      <p class="drinkText">Drinks</p>
-      <div class="drinkContext">
+<section class="menu">
+   <div class="page">
+      <h2>Drinks</h2>
+      <div class="menuContext">
          <div class="menuGrid">
-            <p>Cappuccino</p><p class="lastMenu">2.50 €</p>
-            <p>Expresso Double</p><p class="lastMenu">3.00 €</p>
-            <p>Latte</p><p class="lastMenu">1.50 €</p>
-            <p>The Chaud</p><p class="lastMenu">2.00 €</p>
-            <p>The Glace</p><p class="lastMenu">2.00 €</p>
-            <p>Un Soda</p><p class="lastMenu">2.00 €</p>
-            <p>Le Lait</p><p class="lastMenu">2.00 €</p>
-            <p>Lait au Chocolat</p><p class="lastMenu">2.00 €</p>
+            <div>
+               <p>Cappuccino</p>
+               <p>Expresso Double</p>
+               <p>Latte</p>
+               <p>The Chaud</p>
+               <p>The Glace</p>
+               <p>Un Soda</p>
+               <p>Le Lait</p>
+               <p>Lait au Chocolat</p>
+            </div>
+            <div>
+               <p>2.50€</p>
+               <p>3.00€</p>
+               <p>1.50€</p>
+               <p>2.00€</p>
+               <p>2.00€</p>
+               <p>2.00€</p>
+               <p>2.00€</p>
+               <p>2.00€</p>
+            </div>
          </div>
       </div>
    </div>
-   <div class="bakedGoods">
-      <p class="bakedGoodsText">Baked Goods</p>
-      <div class="bakedGoodsContext">
+   <div class="page">
+      <h2>Baked Goods</h2>
+      <div class="menuContext">
          <div class="menuGrid">
-            <p>Le Petit Dejeuner</p><p class="lastMenu">2.50 €</p>
-            <p>Croissant au Jambon</p><p class="lastMenu">3.00 €</p>
-            <p>Croissant</p><p class="lastMenu">1.50 €</p>
-            <p>Pain au Chocolat</p><p class="lastMenu">2.00 €</p>
-            <p>Croissant aux Pommes</p><p class="lastMenu">2.00 €</p>
-            <p>Tartare de Fruits</p><p class="lastMenu">2.00 €</p>
-            <p>Macarons</p><p class="lastMenu">2.00 €</p>
-            <p>Baguette</p><p class="lastMenu">2.00 €</p>
+            <div>
+               <p>Le Petit Dejeuner</p>
+               <p>Croissant au Jambon</p>
+               <p>Croissant</p>
+               <p>Pain au Chocolat</p>
+               <p>Croissant aux Pommes</p>
+               <p>Tartare de Fruits</p>
+               <p>Macarons</p>
+               <p>Baguette</p>
+            </div>
+            <div>
+               <p>2.50€</p>
+               <p>3.00€</p>
+               <p>1.50€</p>
+               <p>2.00€</p>
+               <p>2.00€</p>
+               <p>2.00€</p>
+               <p>2.00€</p>
+               <p>2.00€</p>
+            </div>
          </div>
       </div>
    </div>
-</div>
+</section>
