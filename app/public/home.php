a419ef239c64ccddd1f0af45af57d18e87f3a401
JoeyKraemer <joey.kramer@student.nhlstenden.com>
2022-10-26 17:38:17 +0200

    made changes to prepare merge

---
+++ app/public/home.php
@@ -0,0 +1,24 @@
+<!-- heroBanner -->
+<section class="heroBanner">
+   <h1>café avec une touche française</h1>
+</section>
+<!-- products -->
+<section class="products">
+   <h2>Our french products</h2>
+   <div class="product">
+      <img src="images/croissant1.jpg" alt="croissant francais">
+      <h3>Croissant</h3>
+   </div>
+   <div class="product">
+      <img src="images/coffeeBeans1.jpg" alt="coffee francais">
+      <h3>Coffee</h3>
+   </div>
+   <div class="product">
+      <img src="images/baguette.jpeg" alt="baguette francais">
+      <h3>Baguette</h3>
+   </div>
+   <div class="product">
+      <img src="images/macaron1.jpg" alt="macaron francais">
+      <h3>Macaron</h3>
+   </div>
+</section>
