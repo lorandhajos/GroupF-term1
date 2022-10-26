0e28ac403be2eea45228bf14398a624032a428ff
Iarina <112045565+iarinamd@users.noreply.github.com>
2022-10-26 21:33:58 +0200

    first page of learn more

---
+++ app/public/learnmore.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>OUI</title>
+   <link rel="stylesheet" href="style/styles.css">
+</head>
+<header>
+   <nav></nav>
+</header>
+
+   <div class="containerLearnMore">
+   <div class = "box">
+      <div class="ingredients">
+         <h1 class="recipieText1">Ingredients</h1>
+            <div class="recipieIngredients">
+               <p><b>25ml</b> Kahlua</p>
+               <p><b>25ml</b> Cointreau</p>
+               <p><b>1 cup</b> hot coffee</p>
+               <p><b>1 tsp.</b> sugar</p>
+               <p>Whipped cream</p>
+               <p>Chocolate shaving,for granish - optional</p>
+            </div>
+      </div>
+      <div class="preparation">
+         <h2 class="recipieText2">How to make</h2>
+            <div class="recipiePreparation">
+               <p style="font-size:27px"><b><i>Step One</i></b></p>
+               <p>In a glass mug mix togheter kahlua, cointreau and coffee.</p>
+               <p>Stir in sugar.</p>
+               <p style="font-size:27px"><b><i>Step Two</i></b></p>
+               <p>Top with whipped cream and chocolate shavings.</p>
+            </div>
+      </div>
+      </div>
+      <div class="video">
+         <iframe width="560" height="315" src="https://www.youtube.com/embed/5HhWpvp55Zk" title="YouTube video player" frameborder="0"
+         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+         </iframe>
+      </div>
+   </div>
+<footer>
+
+</footer>
+
+</html>
