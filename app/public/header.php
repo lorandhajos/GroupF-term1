66802fc1805c8ca644c3f9909ed62bd19b49fb42
iamjames5 <lennox.james@student.nhlstenden.com>
2022-10-26 14:08:35 +0200

    Header mobile version

--- app/public/header.php
+++ app/public/header.php
@@ -1,19 +1,25 @@
 <!DOCTYPE html>
 <html>
+	<meta charset=UTF-8>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
    <link rel="stylesheet" href="style/styles.css">
-   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Caveat">
-
    <div class="gridContainer">
       <header>
          <img id="logo" src="images/logo.svg" alt="logo">
          <nav>
-            <ul> 
-               <li><a href="menu.php">Menu</a></li>
-               <li><a href="findUs.php">Find Us</a></li>
-               <li><a href="aboutUs.php">About Us</a></li>
-               <li><a href="instruction.php">Instruction</a></li>
+            <ul class="nav-menu"> 
+               <li class="nav-item"><a href="menu.php" class="nav-link">Menu</a></li>
+               <li class="nav-item"><a href="findUs.php" class="nav-link">Find Us</a></li>
+               <li class="nav-item"><a href="aboutUs.php" class="nav-link">About Us</a></li>
+               <li class="nav-item"><a href="instruction.php" class="nav-link">Instruction</a></li>
             </ul>
          </nav>
+         <div class="hamburger">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+         </div>
       </header>
+		<script src="script/hamburger.js"></script>
    </div>
 </html>
