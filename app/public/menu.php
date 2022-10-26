a67a02f3b72c4905c8bb59c66285be7b1447a68e
Iarina <112045565+iarinamd@users.noreply.github.com>
2022-10-26 12:16:41 +0200

    fix indentation thank u lorand

--- app/public/menu.php
+++ app/public/menu.php
@@ -16,30 +16,30 @@
                 <p class="drinkText">Drinks</p>
                 <div class="drinkContext">
 				   <div class="menuGrid">
-						<p>Cappuccino</p><p class="lastMenu">2.50 €</p>
-                        <p>Expresso Double</p><p class="lastMenu">3.00 €</p>
-                        <p>Latte</p><p class="lastMenu">1.50 €</p>
-                        <p>The Chaud</p><p class="lastMenu">2.00 €</p>
-                        <p>The Glace</p><p class="lastMenu">2.00 €</p>
-                        <p>Un Soda</p><p class="lastMenu">2.00 €</p>
-                        <p>Le Lait</p><p class="lastMenu">2.00 €</p>
-                        <p>Lait au Chocolat</p><p class="lastMenu">2.00 €</p>
-					</div>
+					   <p>Cappuccino</p><p class="lastMenu">2.50 €</p>
+					   <p>Expresso Double</p><p class="lastMenu">3.00 €</p>
+					   <p>Latte</p><p class="lastMenu">1.50 €</p>
+					   <p>The Chaud</p><p class="lastMenu">2.00 €</p>
+					   <p>The Glace</p><p class="lastMenu">2.00 €</p>
+					   <p>Un Soda</p><p class="lastMenu">2.00 €</p>
+					   <p>Le Lait</p><p class="lastMenu">2.00 €</p>
+					   <p>Lait au Chocolat</p><p class="lastMenu">2.00 €</p>
+				   </div>
                 </div>
             </div>
             <div class="bakedGoods">
                 <p class="bakedGoodsText">Baked Goods</p>
                 <div class="bakedGoodsContext">
-                    <div class="menuGrid">
-                        <p>Le Petit Dejeuner</p><p class="lastMenu">2.50 €</p>
-						<p>Croissant au Jambon</p><p class="lastMenu">3.00 €</p>
-						<p>Croissant</p><p class="lastMenu">1.50 €</p>
-						<p>Pain au Chocolat</p><p class="lastMenu">2.00 €</p>
-						<p>Croissant aux Pommes</p><p class="lastMenu">2.00 €</p>
-						<p>Tartare de Fruits</p><p class="lastMenu">2.00 €</p>
-						<p>Macarons</p><p class="lastMenu">2.00 €</p>
-						<p>Baguette</p><p class="lastMenu">2.00 €</p>
-                    </div>
+				   <div class="menuGrid">
+					   <p>Le Petit Dejeuner</p><p class="lastMenu">2.50 €</p>
+					   <p>Croissant au Jambon</p><p class="lastMenu">3.00 €</p>
+					   <p>Croissant</p><p class="lastMenu">1.50 €</p>
+					   <p>Pain au Chocolat</p><p class="lastMenu">2.00 €</p>
+					   <p>Croissant aux Pommes</p><p class="lastMenu">2.00 €</p>
+					   <p>Tartare de Fruits</p><p class="lastMenu">2.00 €</p>
+					   <p>Macarons</p><p class="lastMenu">2.00 €</p>
+					   <p>Baguette</p><p class="lastMenu">2.00 €</p>
+				   </div>
                 </div>
             </div>
         </div>
