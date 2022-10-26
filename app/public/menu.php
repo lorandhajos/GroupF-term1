2eb7cdb5a49033181bfc06bfa586fda8e53986e6
Lorand Hajos <lorand.hajos@student.nhlstenden.com>
2022-10-26 23:56:49 +0200

    added titles to pages

--- app/public/menu.php
+++ app/public/menu.php
@@ -1,34 +1,37 @@
 <?php if(!defined("OUI")) exit; ?>
 
 <section class="menu">
-   <div class="page">
-      <h2>Drinks</h2>
-      <div class="menuContext">
-         <ul>
-            <li><span>Cappuccino</span><span>2.50€</span></li>
-            <li><span>Expresso Double</span><span>3.00€</span></li>
-            <li><span>Latte</span><span>1.50€</span></li>
-            <li><span>The Chaud</span><span>2.00€</span></li>
-            <li><span>The Glace</span><span>2.00€</span></li>
-            <li><span>Un Soda</span><span>2.00€</span></li>
-            <li><span>Le Lait</span><span>2.00€</span></li>
-            <li><span>Lait au Chocolat</span><span>2.00€</span></li>
-         </ul>
+   <h1>Menu</h1>
+   <div class="menuFlex">
+      <div class="page">
+         <h3>Drinks</h3>
+         <div class="menuContext">
+            <ul>
+               <li><span>Cappuccino</span><span>2.50€</span></li>
+               <li><span>Expresso Double</span><span>3.00€</span></li>
+               <li><span>Latte</span><span>1.50€</span></li>
+               <li><span>The Chaud</span><span>2.00€</span></li>
+               <li><span>The Glace</span><span>2.00€</span></li>
+               <li><span>Un Soda</span><span>2.00€</span></li>
+               <li><span>Le Lait</span><span>2.00€</span></li>
+               <li><span>Lait au Chocolat</span><span>2.00€</span></li>
+            </ul>
+         </div>
       </div>
-   </div>
-   <div class="page">
-      <h2>Baked Goods</h2>
-      <div class="menuContext">
-         <ul>
-            <li><span>Le Petit Dejeuner</span><span>2.50€</span></li>
-            <li><span>Croissant au Jambon</span><span>3.00€</span></li>
-            <li><span>Croissant</span><span>1.50€</span></li>
-            <li><span>Pain au Chocolat</span><span>2.00€</span></li>
-            <li><span>Croissant aux Pommes</span><span>2.00€</span></li>
-            <li><span>Tartare de Fruits</span><span>2.00€</span></li>
-            <li><span>Macarons</span><span>2.00€</span></li>
-            <li><span>Baguette</span><span>2.00€</span></li>
-         </ul>
+      <div class="page">
+         <h3>Baked Goods</h3>
+         <div class="menuContext">
+            <ul>
+               <li><span>Le Petit Dejeuner</span><span>2.50€</span></li>
+               <li><span>Croissant au Jambon</span><span>3.00€</span></li>
+               <li><span>Croissant</span><span>1.50€</span></li>
+               <li><span>Pain au Chocolat</span><span>2.00€</span></li>
+               <li><span>Croissant aux Pommes</span><span>2.00€</span></li>
+               <li><span>Tartare de Fruits</span><span>2.00€</span></li>
+               <li><span>Macarons</span><span>2.00€</span></li>
+               <li><span>Baguette</span><span>2.00€</span></li>
+            </ul>
+         </div>
       </div>
    </div>
 </section>
