f77a895a99d45a037a9f5754d19499bb815cc552
iamjames5 <lennox.james@student.nhlstenden.com>
2022-10-27 15:03:30 +0200

    Added line to the menu

--- app/public/header.php
+++ app/public/header.php
@@ -11,14 +11,14 @@
                <li class="nav-item"><a href="menu.php" class="nav-link">Menu</a></li>
                <li class="nav-item"><a href="findUs.php" class="nav-link">Find Us</a></li>
                <li class="nav-item"><a href="aboutUs.php" class="nav-link">About Us</a></li>
-               <li class="nav-item"><a href="instruction.php" class="nav-link">Instruction</a></li>
+               <li class="nav-item"><a href="instruction.php" class="nav-link" id="mobileLine">Instruction</a></li>
             </ul>
          </nav>
-         <div class="hamburger">
-            <span class="bar"></span>
-            <span class="bar"></span>
-            <span class="bar"></span>
-         </div>
+            <div class="hamburger">
+               <span class="bar"></span>
+               <span class="bar"></span>
+               <span class="bar"></span>
+            </div>
       </header>
 		<script src="script/hamburger.js"></script>
    </div>
