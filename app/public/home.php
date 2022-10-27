cb20ce6381d315ae59713eacb4e6836b63ca224e
Andrii Chumakov <andrii.chumakov@student.nhlstenden.com>
2022-10-27 20:23:18 +0200

    added instructions section to the home page

--- app/public/home.php
+++ app/public/home.php
@@ -26,3 +26,18 @@
       </div>
    </div>
 </section>
+<!--Instructions section-->
+<section class="homeInstructionsBox">
+   <h2 class="instructionsTitle">Instructions</h2>
+   <div class="instructionsFlex">
+      <img src="images/bakeCroissant.jpg" alt="Making croissants" class="bakeCroissantsImg">
+      <div class="textImgFlex">
+         <div>
+            <p>Want to bake croissants like we do?</p>
+            <p>Then sign up for our lessons.</p>
+			<!--insert the lessons page into location.href = '' -->
+         </div>
+         <button type="button" class="toLessonsBtn" onclick="location.href='index.php?page=home'">Go to lessons</button>
+      </div>
+   </div>
+</section>
